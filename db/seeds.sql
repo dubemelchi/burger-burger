ba948013a96270a166832a5451336de413765295
dubemelchi <dubemelchi@gmail.com>
2019-08-17 07:27:59 -0400

    burger app redo. init file setup and mass push

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burger_db;
+INSERT INTO burgers (burger_name) VALUES ('Burger Town Special');
+INSERT INTO burgers (burger_name) VALUES ('Baconantor Be Back');
+INSERT INTO burgers (burger_name) VALUES ('WhoopeR);
